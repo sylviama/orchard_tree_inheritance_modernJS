af273de5ff64583ed0033f06dc81510a10062b7c
Sylvia Ma <xiangwma@umich.edu>
2016-04-16 13:34:22 -0500

    change into using this, but cannot use callbackFunctions, so no random integar generation

--- orchard_tree.js
+++ orchard_tree.js
@@ -1,50 +1,50 @@
 "use strict"
 
 var Plant= function(){;
-	this.height = 0;
-	this.increaseHeight = function(growth){
-		this.height += growth;
-	};
-	this.decreaseHeight = function(amount){
-		height-= amount;
-	};
+	this.height= 0;
+	this.branches =0;
+	this.name= null;
 };
 
-var Tree= function(){};
+Plant.prototype.increaseHeight = function(amount){
+	this.height += amount;
+	$("#tree").append("<div>"+this.name+" tree is now: "+this.height+" inches tall, and has "+this.branches+" branches</div>");
+	};
 
-Tree.prototype= new Plant();
+Plant.prototype.decreaseHeight = function(amount){
+	this.height-= amount;
+};
 
 
-Tree= function(){
-	var branches =0;
-	var height =0;
-	var above10Height=0;
-	this.grow = function(amount,name){
-	height+=amount;
+var Tree= function(){
+	this.branches =0;
+	this.above10Height=0;
+	this.name= null;
+};
 
+Tree.prototype = new Plant();
+
+Tree.prototype.grow = function(amount){
+	this.increaseHeight(amount);
 
 	//grow by 10, gain one more branch
-	above10Height+=amount;
-	while (above10Height>=10){
-		branches+=1;
-		above10Height-=10;
+	this.above10Height+=amount;
+	while (this.above10Height>=10){
+		this.branches+=1;
+		this.above10Height-=10;
 	};
+}
+
+Tree.prototype.trim = function(amount,name){
+	branches -= 1;
+	this.height -= amount;
 
 	$("#tree").append("<div>"+name+" tree is now: "+height+" inches tall, and has "+branches+" branches</div>");
-	}
-
-	//trim function
-	this.trim = function(amount,name){
-		branches -= 1;
-		this.height -= amount;
-
-		$("#tree").append("<div>"+name+" tree is now: "+height+" inches tall, and has "+branches+" branches</div>");
-		// console.log("Trimed!");
-	};
+	// console.log("Trimed!");
 };
 
 
-// Tree.prototype = new Plant();
+
 
 var PearTree = new Tree();
 PearTree.name = "Pear";
@@ -54,6 +54,7 @@ OakTree.name = "Oak";
 
 //generate integar, give the small one to pear and larger to oak
 function integarGenerator(callBackFunction1, callBackFunction2){
+// function integarGenerator(){
 	var pearInt =0;
 	var oakInt =0;
 
@@ -67,12 +68,14 @@ function integarGenerator(callBackFunction1, callBackFunction2){
 		pearInt = int2;
 		oakInt = int1;
 	}
-
+  
 	// console.log("pearInt: ", pearInt);
 	// console.log("oakInt: ", oakInt);
 	//call either the grow or trim function
-	callBackFunction1(pearInt, PearTree.name);
-	callBackFunction2(oakInt, OakTree.name);
+	// PearTree.grow(pearInt);
+	// OakTree.grow(oakInt);
+	callBackFunction1(pearInt);
+	callBackFunction2(oakInt);
 	$("#tree").append("<br>");
 };
 
@@ -84,14 +87,21 @@ var growCount =0;
 var above10Count =0;
 
 function treeStrategy(){
-	integarGenerator(PearTree.grow, OakTree.grow);
+	// integarGenerator(PearTree.grow, OakTree.grow);
+	PearTree.grow(7);
+	OakTree.grow(8);
+	$("#tree").append("<br>");
+
+	// integarGenerator();
 	growCount+=1;
 
 	//every 10 time trigger trim
 	above10Count+=1;
 	if(above10Count>=10){
 		above10Count-=10;
-		integarGenerator(PearTree.trim, OakTree.trim);
+		PearTree.trim(4);
+		OakTree.trim(6);
+		// integarGenerator(PearTree.trim, OakTree.trim);
 	}
 
 	//max grow 30 times
